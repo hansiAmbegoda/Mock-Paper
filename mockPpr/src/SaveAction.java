96c248347972b6e2c96c050cd251a770757c879a
Rashini Hansika <80822074+RashiniHansika@users.noreply.github.com>
2022-09-11 01:42:37 +0530

    OOP mock ppr

---
+++ mockPpr/src/SaveAction.java
@@ -0,0 +1,13 @@
+
+public class SaveAction implements Action{
+	Receiver receiver;
+	
+	public SaveAction(Receiver receiver) {
+		this.receiver = receiver;
+	}
+	
+	public void Do() {
+		receiver.PerformSave();
+	}
+
+}
---
+++ mockPpr/src/SaveAllAction.java
@@ -0,0 +1,12 @@
+
+public class SaveAllAction implements Action{
+	Receiver receiver;
+	
+	public SaveAllAction(Receiver receiver) {
+		this.receiver = receiver;
+	}
+	
+	public void Do() {
+		receiver.PerformSaveAll();
+	}
+}
